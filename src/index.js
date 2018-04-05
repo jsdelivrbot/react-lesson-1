5cf19609ea63de6860cf597129449c86276f8fb2
E6w1nLee <night0097@gmail.com>
2018-04-05 21:11:57 +0800

    L16

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAnXtrp3aATTq1AooLkon4-syjzB-tXswU';
+
+
+// component(s) pruduce some html
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar /> Go!
+        </div>
+    );
+}
+
+//generate page DOM
+ 
+ReactDOM.render(<App />,document.querySelector('.container'));
