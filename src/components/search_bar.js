e7caaec1b42f50bc518e8a4d0156492c1ed12006
E6w1nLee <night0097@gmail.com>
2018-04-07 14:34:33 +0800

    L20.2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,7 @@ class SearchBar extends Component {
         return (
             <div>
                 <input 
-                value={this.state.term} //
+                value={this.state.term}////
                 onChange={(event) => this.setState({term:event.target.value})} />
             </div>
         );
