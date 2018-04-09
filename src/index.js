0057f824ec2a0b14585b27dbce22c785feb8977c
E6w1nLee <night0097@gmail.com>
2018-04-09 08:40:52 +0800

    L21

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAnXtrp3aATTq1AooLkon4-syjzB-tXswU';
 
 
+imp
+
 // component(s) pruduce some html
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar /> Go!!
-        </div>
-    );
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY,term: 'iphone'},function(data){
+            //this.setState({videos: data});
+            
+        });
+
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar /> Go!!
+            </div>
+        );
+    }
 }
 
 //generate page DOM
